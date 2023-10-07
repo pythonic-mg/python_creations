66e03545bf6925376ccf6df6876b07924c25ca9b
chloe-dev <chloe.devops@gmail.com>
2023-10-07 08:55:50 -0500

    Remove More Integer Enteries

--- Count Words in Language of Flowers/script.py
+++ Count Words in Language of Flowers/script.py
@@ -28,14 +28,17 @@ for lst in list_of_lists_of_words:
         list_of_lists_of_words.remove(lst)
     else:
         for word in lst:
-            if word == word.isdigit():
-                lst.remove(word)
             word = word.strip(string.punctuation)
             word = word.lower()
             if word in word_and_count:
                 word_and_count[word] += 1
             else:
                 word_and_count[word] = 1
+                
+# slowly removing integer entries from the dictionary, but a few still remain
+for key, value in list(word_and_count.items()):
+    if value == isinstance(value, int):
+        del word_and_count[key]
 
 with open("word_and_count", "w") as fileref:
     for word, count in sorted(word_and_count.items()):
