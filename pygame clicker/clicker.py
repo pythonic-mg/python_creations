91e6b936ab061b87ac04388033fda9fa611d4813
chloe-dev <chloe.devops@gmail.com>
2024-03-27 11:12:16 -0500

    Create Persistance

--- pygame clicker/clicker.py
+++ pygame clicker/clicker.py
@@ -1,4 +1,4 @@
-import pygame, sys, time
+import pygame, json
 from pygame.locals import *
 
 def main():
@@ -19,13 +19,12 @@ def main():
     background = BLACK
     framerate = 60
     font = pygame.font.SysFont('aptos.ttf', 20)
-    banana_font = pygame.font.SysFont('aptos.ttf', 100)
+    banana_font = pygame.font.SysFont('aptos.ttf', 80)
     buy_font = pygame.font.SysFont('aptos.ttf', 12)
     timer = pygame.time.Clock()
 
     # game variables 
     bananas_per_second = 0
-    bananas = 0 
     
     clicker_production = 0
     field_production = 0
@@ -34,12 +33,9 @@ def main():
     bank_production = 0
     time_production = 0
 
-    clickers_owned = 0
-    fields_owned = 0 
-    mines_owned = 0
-    factories_owned = 0
-    banks_owned = 0
-    time_shipments_owned = 0
+    with open('mydata.json', 'r') as fileref: 
+        data = json.load(fileref)
+    
 
     clicker_base_cost = 2
     field_base_cost = 5
@@ -48,12 +44,16 @@ def main():
     bank_base_cost = 1500
     time_shipment_base_cost = 10000
 
-    clicker_next_cost = int(clicker_base_cost * (1.08 ** clickers_owned))
-    field_next_cost = int(field_base_cost * (1.20 ** fields_owned))
-    mine_next_cost = int(mine_base_cost * (1.43 ** mines_owned)) 
-    factory_next_cost = int(factory_base_cost * (1.35 ** factories_owned))
-    bank_next_cost = int(bank_base_cost * (1.75 ** banks_owned))
-    time_shipment_next_cost = int(time_shipment_base_cost * (1.05 ** time_shipments_owned))
+    clicker_next_cost = int(clicker_base_cost * (1.08 ** data['clickers_owned']))
+    field_next_cost = int(field_base_cost * (1.20 ** data['fields_owned']))
+    mine_next_cost = int(mine_base_cost * (1.43 ** data['mines_owned'])) 
+    factory_next_cost = int(factory_base_cost * (1.35 ** data['factories_owned']))
+    bank_next_cost = int(bank_base_cost * (1.75 ** data['banks_owned']))
+    time_shipment_next_cost = int(time_shipment_base_cost * (1.05 ** data['time_shipments_owned']))
+
+    clicker_production = data['clickers_owned'] * 2
+
+
 
     def draw_rectangle(color, x_coord, y_coord):
         return pygame.draw.rect(screen, color, [x_coord, y_coord, 18, 18], 1)
@@ -68,58 +68,57 @@ def main():
             if event.type == pygame.ACTIVEEVENT:
                 pygame.time.set_timer(pygame.ACTIVEEVENT, 1000)
                 bananas_per_second = clicker_production + field_production + mine_production + factory_production + bank_production + time_production
-                bananas += bananas_per_second
+                data["bananas"] += bananas_per_second
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if banana_rect.collidepoint(event.pos):
-                    bananas += 1
+                    data["bananas"] += 1
                 if clicker_button.collidepoint(event.pos):
-                    if bananas - clicker_next_cost < 0:
-                        clickers_owned += 0
+                    if data["bananas"] - clicker_next_cost < 0:
+                        data['clickers_owned'] += 0
                     else:
-                        bananas -= clicker_next_cost
-                        clickers_owned += 1
-                    clicker_next_cost = int(clicker_base_cost * (1.30 ** clickers_owned))
-                    clicker_production = clickers_owned * 2
+                        data["bananas"] -= clicker_next_cost
+                        data['clickers_owned'] += 1
+                    clicker_next_cost = int(clicker_base_cost * (1.30 ** data['clickers_owned']))
                 if field_button.collidepoint(event.pos):
-                    if bananas - field_next_cost < 0:
-                        fields_owned += 0
+                    if data["bananas"] - field_next_cost < 0:
+                        data['fields_owned'] += 0
                     else:
-                        bananas -= field_next_cost
-                        fields_owned += 1
-                    field_next_cost = int(field_base_cost * (1.30 ** fields_owned))
-                    field_production = fields_owned * 5
+                        data["bananas"] -= field_next_cost
+                        data['fields_owned'] += 1
+                    field_next_cost = int(field_base_cost * (1.30 ** data['fields_owned']))
+                    field_production = data['fields_owned'] * 5
                 if mine_button.collidepoint(event.pos):
-                    if bananas - mine_next_cost < 0:
-                        mines_owned += 0
+                    if data["bananas"] - mine_next_cost < 0:
+                        data['mines_owned'] += 0
                     else:
-                        bananas -= mine_next_cost
-                        mines_owned += 1
-                    mine_next_cost = int(mine_base_cost * (1.30 ** mines_owned))
-                    mine_production = mines_owned * 20
+                        data["bananas"] -= mine_next_cost
+                        data['mines_owned'] += 1
+                    mine_next_cost = int(mine_base_cost * (1.30 ** data['mines_owned']))
+                    mine_production = data['mines_owned'] * 20
                 if factory_button.collidepoint(event.pos):
-                    if bananas - factory_next_cost < 0:
-                        factories_owned += 0
+                    if data["bananas"] - factory_next_cost < 0:
+                        data['factories_owned'] += 0
                     else:
-                        bananas -= factory_next_cost
-                        factories_owned += 1
-                    factory_next_cost = int(factory_base_cost * (1.30 ** factories_owned))
-                    factory_production = factories_owned * 40
+                        data["bananas"] -= factory_next_cost
+                        data['factories_owned'] += 1
+                    factory_next_cost = int(factory_base_cost * (1.30 ** data['factories_owned']))
+                    factory_production = data['factories_owned'] * 40
                 if bank_button.collidepoint(event.pos):
-                    if bananas - bank_next_cost < 0:
-                        banks_owned += 0
+                    if data["bananas"] - bank_next_cost < 0:
+                        data['banks_owned'] += 0
                     else:
-                        bananas -= bank_next_cost
-                        banks_owned += 1
-                    bank_next_cost = int(bank_base_cost * (1.30 ** banks_owned))
-                    bank_production = banks_owned * 100
+                        data["bananas"] -= bank_next_cost
+                        data['banks_owned'] += 1
+                    bank_next_cost = int(bank_base_cost * (1.30 ** data['banks_owned']))
+                    bank_production = data['banks_owned'] * 100
                 if time_button.collidepoint(event.pos):
-                    if bananas - time_shipment_next_cost < 0:
-                        time_shipments_owned += 0
+                    if data["bananas"] - time_shipment_next_cost < 0:
+                        data['time_shipments_owned'] += 0
                     else:
-                        bananas -= time_shipment_next_cost
-                        time_shipments_owned += 1
-                    time_shipment_next_cost = int(time_shipment_base_cost * (1.30 ** time_shipments_owned))
-                    time_production = time_shipments_owned * 200
+                        data["bananas"] -= time_shipment_next_cost
+                        data['time_shipments_owned'] += 1
+                    time_shipment_next_cost = int(time_shipment_base_cost * (1.30 ** data['time_shipments_owned']))
+                    time_production = data['time_shipments_owned'] * 200
             
 
 
@@ -128,14 +127,14 @@ def main():
         bananas_per_second_text = font.render(f"bananas per second: {str(bananas_per_second)}", True, WHITE)
         screen.blit(bananas_per_second_text, [0, 0])
 
-        if bananas < 1000000:
-            banana_holder = bananas
+        if data["bananas"] < 1000000:
+            banana_holder = data["bananas"]
             banana_text = banana_font.render(str(banana_holder), True, WHITE)
-        if bananas > 1000000 and bananas < 1000000000:
-            banana_holder = str(bananas)[:2] + "." + str(bananas)[2:5] + "mil"
+        if data["bananas"] > 1000000 and data["bananas"] < 1000000000:
+            banana_holder = str(data["bananas"])[:2] + "." + str(data["bananas"])[2:5] + "mil"
             banana_text = banana_font.render(str(banana_holder), True, WHITE)
-        if bananas > 1000000000 and bananas < 1000000000000:
-            banana_holder = str(bananas)[:2] + "." + str(bananas)[3:5] + "bil"
+        if data["bananas"] > 1000000000 and data["bananas"] < 1000000000000:
+            banana_holder = str(data["bananas"])[:2] + "." + str(data["bananas"])[3:5] + "bil"
             banana_text = banana_font.render(str(banana_holder), True, WHITE)
 
         
@@ -147,7 +146,7 @@ def main():
         
         # clicker box
             # "Have: " text 
-        clickers_owned_text = font.render(f"clickers: {str(clickers_owned)}", True, MAGENTA)
+        clickers_owned_text = font.render(f"clickers: {str(data['clickers_owned'])}", True, MAGENTA)
         screen.blit(clickers_owned_text, [2, 350])
             # "Cost: " text
         clicker_cost_text = font.render(f"${str(clicker_next_cost)}", True, GREEN)
@@ -160,7 +159,7 @@ def main():
 
         # field box
             # "Have: " text
-        fields_owned_text = font.render(f"fields: {str(fields_owned)}", True, MAGENTA)
+        fields_owned_text = font.render(f"fields: {str(data['fields_owned'])}", True, MAGENTA)
         screen.blit(fields_owned_text, [2, 400])
             # "Cost: " text
         field_next_cost_text = font.render(f"${str(field_next_cost)}", True, GREEN)
@@ -172,7 +171,7 @@ def main():
 
         # mine box
             # "mines: " text
-        mines_owned_text = font.render(f"mines: {str(mines_owned)}", True, MAGENTA)
+        mines_owned_text = font.render(f"mines: {str(data['mines_owned'])}", True, MAGENTA)
         screen.blit(mines_owned_text, [95, 350])
             # "$" text
         mine_next_cost_text = font.render(f"${str(mine_next_cost)}", True, GREEN)
@@ -184,7 +183,7 @@ def main():
 
         # factory box
             # "factories: " text
-        factories_owned_text = font.render(f"factories: {str(factories_owned)}", True, MAGENTA)
+        factories_owned_text = font.render(f"factories: {str(data['factories_owned'])}", True, MAGENTA)
         screen.blit(factories_owned_text, [95, 400])
             # "$" text
         factory_next_cost_text = font.render(f"${str(factory_next_cost)}", True, GREEN)
@@ -196,7 +195,7 @@ def main():
 
         # bank box
             # "banks: " text
-        banks_owned_text = font.render(f"banks: {str(banks_owned)}", True, MAGENTA)
+        banks_owned_text = font.render(f"banks: {str(data['banks_owned'])}", True, MAGENTA)
         screen.blit(banks_owned_text, [180,350])
             # "$" text
         bank_next_cost_text = font.render(f"${str(bank_next_cost)}", True, GREEN)
@@ -208,7 +207,7 @@ def main():
 
         # time box
             # "time boxes: " text
-        time_shipments_owned_text = font.render(f"time boxes: {str(time_shipments_owned)}", True, MAGENTA)
+        time_shipments_owned_text = font.render(f"time boxes: {str(data['time_shipments_owned'])}", True, MAGENTA)
         screen.blit(time_shipments_owned_text, [180, 400])
             # "$" text
         time_shipment_next_cost_text = font.render(f"${str(time_shipment_next_cost)}", True, GREEN)
@@ -217,6 +216,11 @@ def main():
         screen.blit(buy_text, [230, 425])
             #  Button
         time_button = draw_rectangle(WHITE, 230, 420)
+
+    
+        with open("mydata.json", "w") as fileref:
+            json.dump(data, fileref)
+        
 
         pygame.display.flip()
     pygame.quit()
